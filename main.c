dab312a851e0e0bfbd8e9979272b1a229371a64c
ryu <misnevragil@gmail.com>
2024-05-14 14:12:54 -0300

    Adicionei a funcionalidade de salvar o que é escrito dentro de um arquivo de texto

--- main.c
+++ main.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
+
+int main()
+{
+	FILE *fp;
+	int c, i;
+
+	fp = fopen("test.txt", "w");
+
+	while ((c=getchar())!=EOF)
+		putc(c, fp);
+
+	fclose(fp);
+
 /*
-1º setar as variáveis
-2º abrir e nomear arquivo
+Tenho que fazer o programa ler entrada e salvar em um arquivo de texto(feito)
+Adicionar informação de data e horas quando escrito/salvo.
+Não deixar novos logs substuirem outros por cima.
 */
-main(){
-	int carga, reps;		
-	
 }
