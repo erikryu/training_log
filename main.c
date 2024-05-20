8babcc8e843dd5a823ad85f034e28f2e2be5a7ae
ryu <erikryuguimaraeslima@gmail.com>
2024-05-20 18:45:39 -0300

    Programa já salva informação de data e hora

--- main.c
+++ main.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
+#include <time.h>
 
 int main()
 {
 	FILE *fp;
-	int c, i;
+	time_t t = time(NULL);
+	struct tm date = *localtime(&t);
 
-	fp = fopen("test.txt", "w");
+	char dateTime[200];
+	strftime(dateTime, 200, "%c", &date);
 
-	while ((c=getchar())!=EOF)
-		putc(c, fp);
-
+	fp = fopen("tests.txt", "w");
+	fputs(dateTime, fp);
 	fclose(fp);
-
 /*
 Tenho que fazer o programa ler entrada e salvar em um arquivo de texto(feito)
-Adicionar informação de data e horas quando escrito/salvo.
+Adicionar informação de data e horas quando escrito/salvo.(feito)
 Não deixar novos logs substuirem outros por cima.
+Formatação do arquivo deverá ficar em JSON para póstumas alterações
 */
 }
